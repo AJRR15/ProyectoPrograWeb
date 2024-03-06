0907cefd9c1071755761d5beba798bde3338b1cc
AJRR15 <arivero54409@ufide.ac.cr>
2024-03-05 23:10:36 -0600

    Version Catalogo

---
+++ Proyecto/src/main/java/com/Proyecto/Proyecto/controller/JuegosController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Proyecto.Proyecto.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+/**
+ *
+ * @author alejh
+ */
+
+
+
+
+@Controller
+public class JuegosController {
+
+    @GetMapping("/juegos")
+    public String mostrarJuegos() {
+        return "juego/juegos";
+    }
+}
