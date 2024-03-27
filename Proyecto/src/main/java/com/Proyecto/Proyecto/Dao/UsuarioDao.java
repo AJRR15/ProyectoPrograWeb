41ab2ea3a377b071956ef21ad0d2c9bf9d796c6e
AJRR15 <arivero54409@ufide.ac.cr>
2024-03-27 15:17:11 -0600

    Proyecto 60% Jose

---
+++ Proyecto/src/main/java/com/Proyecto/Proyecto/Dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Proyecto.Proyecto.Dao;
+
+//import com.Proyecto.Proyecto.Domain.Usuario;
+import com.Proyecto.Proyecto.Domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao  {
+    
+}
+//extends JpaRepository <Usuario, Long> {
+//    Usuario findByUsername(String username);
+//    
+//}
