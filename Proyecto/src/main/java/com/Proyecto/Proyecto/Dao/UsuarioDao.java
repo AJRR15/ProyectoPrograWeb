c1bf67726028b896a514d86434d857d5fcb70067
AJRR15 <arivero54409@ufide.ac.cr>
2024-04-20 00:10:53 -0600

    Seguridad Funcional

--- Proyecto/src/main/java/com/Proyecto/Proyecto/Dao/UsuarioDao.java
+++ Proyecto/src/main/java/com/Proyecto/Proyecto/Dao/UsuarioDao.java
@@ -1,17 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.Proyecto.Proyecto.Dao;
 
-//import com.Proyecto.Proyecto.Domain.Usuario;
-import com.Proyecto.Proyecto.Domain.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.Proyecto.Proyecto.Domain.Usuario;
 
-public interface UsuarioDao  {
+public interface UsuarioDao extends JpaRepository <Usuario, Long> {
+    Usuario findByUsername(String username);
+    Usuario findByUsernameAndPassword(String username, String Password);
+    Usuario findByUsernameOrCorreo(String username, String Correo);
+    boolean existsByUsernameOrCorreo(String username, String Correo);
+    
     
 }
-//extends JpaRepository <Usuario, Long> {
-//    Usuario findByUsername(String username);
-//    
-//}
