78bee89cd602b0fc13020719130b7b1350cef567
AJRR15 <arivero54409@ufide.ac.cr>
2024-04-24 16:15:08 -0600

    Borrar Boton agregar

--- Proyecto/src/main/java/com/Proyecto/Proyecto/controller/ContactosController.java
+++ Proyecto/src/main/java/com/Proyecto/Proyecto/controller/ContactosController.java
@@ -2,6 +2,7 @@
 package com.Proyecto.Proyecto.controller;
 
 import com.Proyecto.Proyecto.Domain.Contactos;
+import com.Proyecto.Proyecto.Domain.Informacion;
 import com.Proyecto.Proyecto.Service.ContactosService;
 import com.Proyecto.Proyecto.Service.InformacionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +42,17 @@ public class ContactosController {
         contactosService.delete(contacto);
         return "redirect:/contacto/contactos";
     }
+
+    @PostMapping("/guardarInformacion")
+    public String guardarInformacion(Informacion informacion) {
+        infoService.save(informacion);
+        return "redirect:/contacto/contactos";
+    }
+
+    @GetMapping("/eliminarInformacion/{id_informacion}")
+    public String informacionEliminar(Informacion Informacion) {
+        infoService.delete(Informacion);
+        return "redirect:/contacto/contactos";
+    }
+
 }
